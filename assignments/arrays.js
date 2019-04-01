8733a341998471f70de2adc4889f3be3b92e64ca
amlane <twelvexstring@gmail.com>
2019-04-01 17:09:51 -0500

    finished working on array.js assignment

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -71,15 +71,15 @@ console.log(
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log(`The last car is a ${inventory[inventory.length-1].car_make} ${inventory[inventory.length-1].car_model}`);
+console.log(`The last car in the inventory is a ${inventory[inventory.length-1].car_make} ${inventory[inventory.length-1].car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-
+let carModels = [];
 for (let i = 0; i < inventory.length; i++){
-    let carModels = [`${inventory[i].car_model}`];
-    console.log(carModels.sort()); //cannot get to alphabetize
+    carModels.push(inventory[i].car_model);
 };
+console.log(carModels.sort());
 
 // MyData.sort(dynamicSort("city"));
 
@@ -94,14 +94,16 @@ for (let i = 0; i < inventory.length; i++) {
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 
-
-let oldCars = [];
-console.log(); 
+const result = inventory.filter(inventory => inventory.car_year < 2000);
+console.log(result.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
-console.log();
+
+let BMWAndAudi = inventory.filter(
+  inventory => inventory.car_make === "BMW" || inventory.car_make === "Audi"
+);
+console.log(BMWAndAudi);
 
 
 
