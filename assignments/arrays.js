06a68d22943fdf6100130a3e7f116caf28c46d9d
amlane <twelvexstring@gmail.com>
2019-04-01 17:19:40 -0500

    refactored challenge 5 to include the variable it requested

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -95,8 +95,11 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 
-const result = inventory.filter(inventory => inventory.car_year < 2000);
-console.log(result.length); 
+// const result = inventory.filter(inventory => inventory.car_year < 2000);
+// console.log(result.length); 
+
+const result = carYears.filter(carYears => carYears < 2000);
+console.log(result.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
